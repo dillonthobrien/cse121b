61ba5ffbd173ba370ef45cc184d6192660095fb2
dillonthobrien <obr22001@byui.edu>
2024-01-20 18:35:51 -0700

    removed placeholder and added my own image

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 let fullName = "Dillon O'Brien";
 var currentYear = new Date().getFullYear();
-var profilePicture = "images/me.png";
+var profilePicture = "images/me.jpg";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
